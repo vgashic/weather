b71edc736452752377f104c0a88988525b5bff38
Gasha <vgashic@gmail.com>
2016-07-07 15:26:11 +0200

    layout fixes js fixes css fixes

--- js/main.js
+++ js/main.js
@@ -3,52 +3,62 @@
 
 
 function convertTemperature(val, convertTo) {
-	"use strict";
-	if (convertTo === "C") {
-		return Math.round((val - 32.0) / 1.8);
-	}
+    "use strict";
+    if (convertTo === "C") {
+        return Math.round((val - 32.0) / 1.8);
+    }
 
-	if (convertTo === "F") {
-		return Math.round(val * 1.8 + 32);
-	}
+    if (convertTo === "F") {
+        return Math.round(val * 1.8 + 32);
+    }
 
-	return "n/a";
+    return "n/a";
 }
 
 
 function switchUnits() {
-	"use strict";
-	var checked = $("#cf-switch").is(":checked"),
+    "use strict";
+    var checked = $("#cf-switch").is(":checked"),
 
-		// current values
-		tempVal = $(".temp-val").text(),
-		tempUnit = $(".temp-units").text()[1];
+        // current values
+        tempVal = $(".temp-val").text(),
+        tempUnit = $(".temp-units").text()[1];
 
-	if (checked) {
-		$(".temp-units").text("°C");
-		$(".temp-val").text(convertTemperature(tempVal, "C"));
-	} else {
-		$(".temp-units").text("°F");
-		$(".temp-val").text(convertTemperature(tempVal, "F"));
-	}
+    if (checked) {
+        $(".temp-units").text("°C");
+        $(".temp-val").text(convertTemperature(tempVal, "C"));
+    } else {
+        $(".temp-units").text("°F");
+        $(".temp-val").text(convertTemperature(tempVal, "F"));
+    }
 }
 
 function changeImages() {
-	"use strict";
+    "use strict";
 
 }
 
 
 function getWeatherData(location) {
-	"use strict";
-	var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/weather?"
-	console.log(location.location.lat);
-	var lat = "lat=" + location.location.lat + "&";
-	var lon = "lon=" + location.location.lng + "&";
-	var appId = "APPID=54b4f28c77ecfb3acd3e3bf77ed99b61";
-	var url = weatherUrl + lat + lon + appId;
+    "use strict";
+    var weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/weather?",
+        lat = "lat=" + location.location.lat + "&",
+        lon = "lon=" + location.location.lng + "&",
+        appId = "APPID=54b4f28c77ecfb3acd3e3bf77ed99b61",
+        url = weatherUrl + lat + lon + appId;
 
-	console.log(url);
+    $.ajax({
+        url: url,
+        type: "POST",
+        dataType: "jsonp",
+        success: function (response) {
+            console.log(response);
+
+            $(".temp-val").text(Math.round(response.list[0].main.temp - 273.15, 0));
+
+            $(".city-name").text(response.city.name);
+        }
+    });
 }
 
 //{
@@ -62,22 +72,23 @@ function getWeatherData(location) {
 
 
 function getLocation() {
-	"use strict";
-	$.ajax({
-		url: "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBM75kiKj9tkuhWe97m1Ckqlq80bc_VduA",
-		type: "POST",
-		success: function (response) {
-			getWeatherData(response);
-		},
-		error: function () {
-			// todo: handle error
-			alert("Can't get your location");
-		}
-	});
+    "use strict";
+    $.ajax({
+        url: "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyBM75kiKj9tkuhWe97m1Ckqlq80bc_VduA",
+        type: "POST",
+        success: function (response) {
+            getWeatherData(response);
+        },
+        error: function () {
+            // todo: handle error
+            alert("Can't get your location");
+        }
+    });
 }
 
 
 $(document).ready(function () {
-	"use strict";
-	$("#cf-switch").on("click", switchUnits);
+    "use strict";
+    getLocation();
+    $("#cf-switch").on("click", switchUnits);
 });
